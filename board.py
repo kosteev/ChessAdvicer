674c799990fc1c451b4149fc4411d53e9223c94f
Eugen Kostieiev <kosteev@gvmachines.com>
2016-12-02 00:14:00 +0200

    sort by take

--- board.py
+++ board.py
@@ -5,7 +5,7 @@ from utils import on_board
 
 
 class Board(object):
-    sort_by_take_value = lambda x: (
+    SORT_BY_TAKE_VALUE = lambda x: (
         -1 if x['new_position_old_piece'] else 1,
         -PIECES[x['new_position_old_piece'][0]]['value'] if x['new_position_old_piece'] else 0)
 
@@ -77,7 +77,7 @@ class Board(object):
 
         random.shuffle(moves)
         if sort_key is None:
-            sort_key = Board.sort_by_take_value
+            sort_key = self.SORT_BY_TAKE_VALUE
         moves.sort(key=sort_key)
 
         for move in moves:
