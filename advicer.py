54325e43cfcb3a7e4efb2b10c1a32ad99b66a7ea
Eugen Kostieiev <kosteev@gvmachines.com>
2016-12-27 23:39:17 +0200

    run analyzer refact

--- advicer.py
+++ advicer.py
@@ -1,9 +1,9 @@
 import time
 
 from analyze import AlphaBetaAnalyzer
-from evaluation import take_evaluation, simple_evaluation
+from evaluation import take_evaluation
 from openings import get_opening_info
-from pieces import get_opp_color, BLACK, WHITE
+from pieces import get_opp_color, BLACK
 from utils import moves_stringify, format_move
 
 
@@ -22,7 +22,7 @@ def run_advicer(mode, max_deep, lines, play, board, board_hashes):
     first_line = analysis['result'][0]
     opening_info = get_opening_info(board)
     if opening_info is not None:
-        opening_analysis = analyzer.analyze(board, moves_to_consider=[opening_info['move']])
+        opening_analysis = run_analyzer(analyzer, board, moves_to_consider=[opening_info['move']])
         # TODO: check moves == []
         opening_first_line = opening_analysis['result'][0]
         if (opening_first_line['moves'] and
@@ -32,17 +32,29 @@ def run_advicer(mode, max_deep, lines, play, board, board_hashes):
                 opening_info['name'], format_move(opening_info['move']))
             first_line = opening_first_line
 
+    # Consider repetition
+#     first_line_moves = first_line['moves']
+#     if first_line_moves:
+#         revert_info = board.make_move(first_line_moves[-1])
+#         if board_hashes.get(board.hash) >= 3:
+#             analysis = run_analyzer(analyzer, board)
+#  
+#         board.revert_move(revert_info)
+
     return first_line
 
 
-def run_analyzer(analyzer, board):
+def run_analyzer(analyzer, board, moves_to_consider=None):
     start_time = time.time()
-    analysis = analyzer.analyze(board)
+    analysis = analyzer.analyze(board, moves_to_consider=moves_to_consider)
     end_time = time.time() - start_time
 
-    print 'Analyzer time = {:.6f}, nodes = {}'.format(end_time, analysis['stats']['nodes'])
-    print 'Per node = {:.3f}ms'.format(1000.0 * end_time / analysis['stats']['nodes'])
-    for ind, line in enumerate(analysis['result']):
+    stats = analysis['stats']
+    result = analysis['result']
+    print
+    print 'Analyzer time = {:.6f}, nodes = {}'.format(end_time, stats['nodes'])
+    print 'Per node = {:.3f}ms'.format(1000.0 * end_time / stats['nodes'])
+    for ind, line in enumerate(result):
         eval_move_color = board.move_color if len(line['moves']) % 2 == 0 else get_opp_color(board.move_color)
         eval_ind = (len(line['moves']) + (1 if board.move_color == BLACK else 0)) / 2 + 1
         print '{}. ({}) {} ({})'.format(
